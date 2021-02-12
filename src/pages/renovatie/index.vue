078033dedac826eb81c228fdfb216324e84eaf4c
NeleDB <nele@littlemissrobot.com>
2021-02-12 20:37:16 +0100

    feat: added renovatie

--- src/pages/renovatie/index.vue
+++ src/pages/renovatie/index.vue
@@ -7,6 +7,24 @@
 
 			<p>{{ $t("renovatie.intro") }}</p>
 		</div>
+
+		<div
+			class="l-container:12/12 l-container:10/12@at:vp-9 l-container--default-spacing u-margin-top:8 u-margin-top:14@at:vp-9 s-md"
+		>
+			<img
+				src="@/assets/images/renovatie/remincare.jpg"
+				:alt="$t('renovatie.image1')"
+			/>
+		</div>
+
+		<div
+			class="l-container:12/12 l-container:6/12@at:vp-9 l-container--default-spacing s-md u-margin-top:8 u-margin-top:14@at:vp-9"
+		>
+			<p>{{ $t("renovatie.alinea1") }}</p>
+			<p>{{ $t("renovatie.alinea2") }}</p>
+			<p>{{ $t("renovatie.alinea3") }}</p>
+			<p>{{ $t("renovatie.alinea4") }}</p>
+		</div>
 	</section>
 </template>
 
